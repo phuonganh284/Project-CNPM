fd59072372aad8a5ff3179c07c6fb6f26c219e7c
Nguyen Phuong <nagiwagi444@gmail.com>
2025-10-20 18:38:08 +0700

    Update Nav links on click

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,63 +1,100 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 const SideBar = () => {
+    const linkBase = "flex items-center space-x-3 cursor-pointer text-lg transition-colors duration-200";
+    const inactiveColor = "text-[#8A8A8A] hover:text-[#4D4D4D]";
+    const activeColor = "text-[#4D4D4D] font-semibold";
+
     return (
         <div
             className="
-                fixed
-                left-6
-                top-6
-                w-[250px]
-                md:w-[280px]
-                h-[calc(100vh-38px)]
-                bg-white
-                rounded-2xl
-                rounded-r-none
-                flex
-                flex-col
-                items-start
-                p-6
-                shadow-md
-                z-10
-            "
+        fixed
+        left-6
+        top-6
+        w-[250px]
+        md:w-[280px]
+        h-[calc(100vh-38px)]
+        bg-white
+        rounded-2xl
+        rounded-r-none
+        flex
+        flex-col
+        items-start
+        p-6
+        shadow-md
+        z-10
+      "
         >
             {/* Logo ------------------------------------------------------------*/}
             <div className="mb-8 mt-[-20px] ml-9">
-                <Link to="/home" className="flex items-center sidebar-link">
+                <NavLink to="/home" className="flex items-center sidebar-link">
                     <img src={assets.logo} alt="Library Logo" className="w-[160px] h-auto cursor-pointer" />
-                </Link>
+                </NavLink>
             </div>
 
             {/* Nav links -------------------------------------------------------*/}
             <nav className="mb-auto mt-4 ml-9">
-                <ul className="flex flex-col space-y-6 text-[#8A8A8A]">
-                    <li className="flex items-center space-x-3 cursor-pointer hover:text-[#4D4D4D] text-lg">
-                        <img src={assets.home_icon} alt="Home Icon" className="w-5 h-5" />
-                        <Link to="/home" className="sidebar-link">Home</Link>
+                <ul className="flex flex-col space-y-6">
+                    <li>
+                        <NavLink
+                            to="/home"
+                            className={({ isActive }) =>
+                                `${linkBase} ${isActive ? activeColor : inactiveColor}`
+                            }
+                        >
+                            <img src={assets.home_icon} alt="Home Icon" className="w-5 h-5" />
+                            <span>Home</span>
+                        </NavLink>
                     </li>
-                    <li className="flex items-center space-x-3 cursor-pointer hover:text-[#4D4D4D] text-lg">
-                        <img src={assets.browse_icon} alt="Browse Icon" className="w-5 h-5" />
-                        <a href="#browse">Browse</a>
+
+                    <li>
+                        <NavLink
+                            to="/browse"
+                            className={({ isActive }) =>
+                                `${linkBase} ${isActive ? activeColor : inactiveColor}`
+                            }
+                        >
+                            <img src={assets.browse_icon} alt="Browse Icon" className="w-5 h-5" />
+                            <span>Browse</span>
+                        </NavLink>
                     </li>
-                    <li className="flex items-center space-x-3 cursor-pointer hover:text-[#4D4D4D] text-lg">
-                        <img src={assets.rules_icon} alt="Rules Icon" className="w-5 h-5" />
-                        <a href="#rules">Rules</a>
+
+                    <li>
+                        <NavLink
+                            to="/rules"
+                            className={({ isActive }) =>
+                                `${linkBase} ${isActive ? activeColor : inactiveColor}`
+                            }
+                        >
+                            <img src={assets.rules_icon} alt="Rules Icon" className="w-5 h-5" />
+                            <span>Rules</span>
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
 
             {/* Footer links ----------------------------------------------------*/}
             <nav className="mb-5 mt-2 ml-9">
-                <ul className="flex flex-col space-y-3 text-[#8A8A8A] text-sm">
-                    <li className="hover:text-[#4D4D4D]">
-                        <Link to="/about" className="sidebar-link">About</Link>
+                <ul className="flex flex-col space-y-3 text-sm">
+                    <li>
+                        <NavLink
+                            to="/about"
+                            className={({ isActive }) =>
+                                `${isActive ? activeColor : inactiveColor}`
+                            }
+                        >
+                            About
+                        </NavLink>
                     </li>
-                    <li className="hover:text-[#4D4D4D]">
-                        <a href="#support">Support</a>
+
+                    <li>
+                        <a href="#support" className={`${inactiveColor}`}>Support</a>
                     </li>
-                    <li className="hover:text-[#4D4D4D]">
-                        <a href="#term-condition">Terms & Condition</a>
+
+                    <li>
+                        <a href="#term-condition" className={`${inactiveColor}`}>Terms & Condition</a>
                     </li>
                 </ul>
             </nav>
